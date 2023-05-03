1c0e39283afa81927eeba2684d2cc9c81628f5ed
rubengardner <rgardnercuesta@gmail.com>
2023-05-03 08:25:25 +0000

    Add routing setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render ={() => <h1>Main page</h1>} />
+          <Route exact path='/login' render ={() => <h1>Log in</h1>} />
+          <Route exact path='/signup' render ={() => <h1>Sign up</h1>} />
+          <Route render={()=><p>Page not found.</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
+import styles from '../styles/NavBar.module.css'
 const NavBar = () => {
   return (
     <Container>
        <Navbar bg="light" expand="md" fixed='top'>
             <Navbar.Brand href="#home">Workout Log</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />  
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Log in</Nav.Link>
-                    <Nav.Link href="#link">Sign up</Nav.Link>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'>Home</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/login'>Log in</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signup'>Sign up</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
