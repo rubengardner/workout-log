4d8040628632ebdf229a057c734ae5aef6f79ea7
rubengardner <rgardnercuesta@gmail.com>
2023-05-03 07:15:05 +0000

    Add basic css to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar/>
     </div>
   );
